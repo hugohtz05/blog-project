2aacf1ba2f974a042dba80a373ecfbeb96a11b8a
hugohtz05 <hugoheintz03@gmail.com>
2024-12-21 20:14:47 +0100

    modification alowed hosts

--- src/blog/settings.py
+++ src/blog/settings.py
@@ -31,7 +31,8 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DEBUG", False)
 
-ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")
+ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=["127.0.0.1", "localhost"])
+
 
 print(SECRET_KEY)
 print(DEBUG)
